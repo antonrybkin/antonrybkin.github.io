05dc64eb7a9730a4b8e4520c7de397225e3e30e4
Anton Rybkin <n1n1@bk.ru>
2018-10-25 02:23:26 -0500

    GH pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,7 +77,7 @@ module.exports = {
 // only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
 const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
   router: {
-    base: '/<repository-name>/'
+    base: '/docs/'
   }
 } : {}
 
